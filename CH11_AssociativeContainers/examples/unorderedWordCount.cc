dce2d7db7b965f4f0e8ff07e513c9951240d58b6
Henry Zhang <zhanghui629@gmail.com>
2016-11-28 14:14:12 +0800

    test the examples codes of ch11.

--- CH11_AssociativeContainers/examples/unorderedWordCount.cc
+++ CH11_AssociativeContainers/examples/unorderedWordCount.cc
@@ -2,44 +2,47 @@
  * This file contains code from "C++ Primer, Fifth Edition", by Stanley B.
  * Lippman, Josee Lajoie, and Barbara E. Moo, and is covered under the
  * copyright and warranty notices given in that book:
- * 
+ *
  * "Copyright (c) 2013 by Objectwrite, Inc., Josee Lajoie, and Barbara E. Moo."
- * 
- * 
+ *
+ *
  * "The authors and publisher have taken care in the preparation of this book,
  * but make no expressed or implied warranty of any kind and assume no
  * responsibility for errors or omissions. No liability is assumed for
  * incidental or consequential damages in connection with or arising out of the
  * use of the information or programs contained herein."
- * 
+ *
  * Permission is granted for this code to be used for educational purposes in
  * association with the book, given proper citation if and when posted or
  * reproduced.Any commercial use of this code requires the explicit written
  * permission of the publisher, Addison-Wesley Professional, a division of
  * Pearson Education, Inc. Send your request for permission, stating clearly
  * what code you would like to use, and in what specific way, to the following
- * address: 
- * 
+ * address:
+ *
  * 	Pearson Education, Inc.
  * 	Rights and Permissions Department
  * 	One Lake Street
  * 	Upper Saddle River, NJ  07458
  * 	Fax: (201) 236-3290
-*/ 
+*/
 
 #include <unordered_map>
 using std::unordered_map;
 
 #include <unordered_set>
-using std::unordered_set; using std::unordered_multiset;
+using std::unordered_set;
+using std::unordered_multiset;
 
 #include <string>
-using std::string; 
+using std::string;
 
 using std::hash;
 
 #include <iostream>
-using std::cin; using std::cout; using std::endl;
+using std::cin;
+using std::cout;
+using std::endl;
 
 #include <cstddef>
 using std::size_t;
@@ -47,38 +50,39 @@ using std::size_t;
 #include "Sales_data.h"
 
 // unordered_map version of the word count program
-int main() 
+int main()
 {
 	// count occurrences, but the words won't be in alphabetical order
-    unordered_map<string, size_t> word_count;  
-    string word;
-    while (cin >> word)
+	unordered_map<string, size_t> word_count;
+	string word;
+	while (cin >> word)
 		++word_count[word]; // fetch and increment the counter for word
 
-	for (const auto &w : word_count) // for each element in the map
+	for (const auto& w : word_count) // for each element in the map
 		// print the results
-		cout <<  w.first << " occurs " << w.second 
-		     << ((w.second > 1) ? " times" : " time") << endl;
+		cout <<  w.first << " occurs " << w.second
+			 << ((w.second > 1) ? " times" : " time") << endl;
 
 	return 0;
 }
 
 // how to override default hash and equality operator on key_type
-size_t hasher(const Sales_data &sd) 
+size_t hasher(const Sales_data& sd)
 {
 	return hash<string>()(sd.isbn());
 }
-bool eqOp(const Sales_data &lhs, const Sales_data &rhs)
+
+bool eqOp(const Sales_data& lhs, const Sales_data& rhs)
 {
 	return lhs.isbn() == rhs.isbn();
 }
 
 // type alias using our functions in place of hash<key_type> and ==
-using SD_multiset = unordered_multiset<Sales_data, 
-                    decltype(hasher)*, decltype(eqOp)*>;
+using SD_multiset = unordered_multiset<Sales_data,
+					decltype(hasher)*, decltype(eqOp)*>;
 
 // bookstore can hold multiple Sales_data with the same ISBN
-// arguments are the bucket size 
+// arguments are the bucket size
 // and pointers to the hash function and equality operator
 SD_multiset bookstore(42, hasher, eqOp);
 
@@ -87,7 +91,7 @@ SD_multiset bookstore(42, hasher, eqOp);
 struct Foo { string s; };
 
 // we'll see how to define our own operators in chapter 14
-bool operator==(const Foo& l, const Foo&r) { return l.s == r.s; }
+bool operator==(const Foo& lhs, const Foo& rhs) { return lhs.s == rhs.s; }
 
 size_t FooHash(const Foo& f) { return hash<string>()(f.s); }
 
