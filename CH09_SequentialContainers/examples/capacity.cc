322ed3cb120fe0282fe75e147b7d13c2a966519c
Henry Zhang <zhanghui629@gmail.com>
2018-02-03 09:36:37 +0800

    test and format the example codes of chapter 09

--- CH09_SequentialContainers/examples/capacity.cc
+++ CH09_SequentialContainers/examples/capacity.cc
@@ -2,30 +2,30 @@
  * This file contains code from "C++ Primer, Fifth Edition", by Stanley B.
  * Lippman, Josee Lajoie, and Barbara E. Moo, and is covered under the
  * copyright and warranty notices given in that book:
- * 
+ *
  * "Copyright (c) 2013 by Objectwrite, Inc., Josee Lajoie, and Barbara E. Moo."
- * 
- * 
+ *
+ *
  * "The authors and publisher have taken care in the preparation of this book,
  * but make no expressed or implied warranty of any kind and assume no
  * responsibility for errors or omissions. No liability is assumed for
  * incidental or consequential damages in connection with or arising out of the
  * use of the information or programs contained herein."
- * 
+ *
  * Permission is granted for this code to be used for educational purposes in
  * association with the book, given proper citation if and when posted or
  * reproduced.Any commercial use of this code requires the explicit written
  * permission of the publisher, Addison-Wesley Professional, a division of
  * Pearson Education, Inc. Send your request for permission, stating clearly
  * what code you would like to use, and in what specific way, to the following
- * address: 
- * 
- * 	Pearson Education, Inc.
- * 	Rights and Permissions Department
- * 	One Lake Street
- * 	Upper Saddle River, NJ  07458
- * 	Fax: (201) 236-3290
-*/ 
+ * address:
+ *
+ *  Pearson Education, Inc.
+ *  Rights and Permissions Department
+ *  One Lake Street
+ *  Upper Saddle River, NJ  07458
+ *  Fax: (201) 236-3290
+*/
 
 #include <vector>
 using std::vector;
@@ -42,9 +42,9 @@ int main()
          << " capacity: "  << ivec.capacity() << endl;
 
     // give ivec 24 elements
-    for (vector<int>::size_type ix = 0; ix != 24; ++ix) 
+    for (vector<int>::size_type ix = 0; ix != 24; ++ix)
          ivec.push_back(ix);
-     
+
     // size should be 24; capacity will be >= 24 and is implementation defined
     cout << "ivec: size: " << ivec.size()
          << " capacity: "  << ivec.capacity() << endl;
@@ -66,12 +66,12 @@ int main()
     cout << "ivec: size: " << ivec.size()
          << " capacity: "  << ivec.capacity() << endl;
 
-	ivec.shrink_to_fit();  // ask for the memory to be returned
+    ivec.shrink_to_fit();  // ask for the memory to be returned
 
     // size should be unchanged; capacity is implementation defined
     cout << "ivec: size: " << ivec.size()
          << " capacity: "  << ivec.capacity() << endl;
 
-return 0;
+    return 0;
 }
 
